64c2c334add06040f35b7de76daa6a1d16ba43e4
Iasonas Kouveliotis <iasonas@Iasonas.local>
2019-01-10 18:18:25 +0200

    initialized contracts

---
+++ contracts/doubleAuction.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.1;
+
+contract doubleAuction {
+  address owner;
+
+  constructor () public {
+    owner = msg.sender;
+  }
+  
+  
+    
+    
+    
+    
+}
---
+++ contracts/virtualNetMeter.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.1;
+
+contract virtualNetMeter {
+    
+  address owner;
+
+  constructor () public {
+    owner = msg.sender;
+  }
+  
+  
+    
+    
+    
+    
+}
