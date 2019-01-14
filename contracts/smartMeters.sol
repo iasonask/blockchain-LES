e4d33b8ee8caa2e44030159ee0d39db9d61edf5e
Iasonas Kouveliotis <iasonas@Iasonas.local>
2019-01-14 21:32:42 +0200

    Added remove meter function

--- contracts/smartMeters.sol
+++ contracts/smartMeters.sol
@@ -1,12 +1,13 @@
 pragma solidity >=0.4.25 <0.6.0;
 
 contract smartMeters {
+
   address owner;
+
   struct meter {
     bool isSubscribed;
     // other relevant parameters
   }
-  address[] metersArr;
   mapping (address => meter) meters;
 
   event deployMeterService(address meterProvider, address meterContract);
@@ -19,15 +20,23 @@ contract smartMeters {
   function insertMeter(address meter_) public onlyOwner {
     require(!meters[meter_].isSubscribed, "Meter already subscribed.");
     meters[meter_].isSubscribed = true;
-    metersArr.push(meter_);
+  }
+
+  function removeMeter(address meter_) public onlyOwner {
+    require(meters[meter_].isSubscribed, "Meter isn't subscribed.");
+    meters[meter_].isSubscribed = false;
   }
 
   function isValidMeter(address meter_) public view returns (bool) {
     return meters[meter_].isSubscribed;
   }
 
+  function destroy() public onlyOwner {
+    selfdestruct(msg.sender);
+  }
+
   modifier onlyOwner {
-    require(msg.sender == owner, "Only retailer can modify!");
+    require(msg.sender == owner, "Only Meter Provider can modify!");
     _;
   }
 }
